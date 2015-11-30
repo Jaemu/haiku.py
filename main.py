e5a6b6944c53ed699a8d97430e02d7d775078df5
Jill Munson <jillian.munson@viacom.com>
2015-11-29 16:51:34 -0800

    run

--- main.py
+++ main.py
@@ -52,5 +52,5 @@ def page_not_found(e):
 	"""Return a custom 404 error."""
 	return 'Sorry, nothing at this URL.', 404
 
-#if __name__ == '__main__':
-#		app.run()
+if __name__ == '__main__':
+		app.run()
